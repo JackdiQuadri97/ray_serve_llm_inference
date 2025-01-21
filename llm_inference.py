b44bd3962bbe794620f49d53f1c535fd030a169d
Giacomo Virginio <59983258+JackdiQuadri97@users.noreply.github.com>
2025-01-21 14:19:56 +0100

    Update llm_inference.py

--- llm_inference.py
+++ llm_inference.py
@@ -5,23 +5,23 @@ import torch
 from transformers import AutoModelForCausalLM, PreTrainedTokenizerFast
 from ray import serve
 
+MODEL_ID = "Aleph-Alpha/Pharia-1-LLM-7B-control-aligned-hf"
+MAX_NEW_TOKENS = 50
+
+
 @serve.deployment()
 class LLM:
-    def __init__(self, model_id: str, max_new_tokens: int):
-        self.max_new_tokens = max_new_tokens
+    def __init__(self):
         self.tokenizer = PreTrainedTokenizerFast.from_pretrained(MODEL_ID)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print(f"Using device: {self.device}")
-        self.model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, torch_dtype=torch.bfloat16).to(self.device)
+        self.model = AutoModelForCausalLM.from_pretrained(MODEL_ID, trust_remote_code=True, torch_dtype=torch.bfloat16).to(self.device)
 
     def __call__(self, request: Request) -> Dict:
         input_text = request.query_params["text"]
         inputs = self.tokenizer(input_text, return_token_type_ids=False, return_tensors="pt").to(self.device)
-        outputs = self.model.generate(**inputs, max_new_tokens=self.max_new_tokens)
+        outputs = self.model.generate(**inputs, max_new_tokens=MAX_NEW_TOKENS)
         generated_text = self.tokenizer.decode(outputs[0])
         return {"response": generated_text}
 
-app = LLM.bind(
-    model_id=os.environ.get('MODEL_ID')
-    max_new_tokens=os.environ.get('MAX_NEW_TOKENS')
-)
+app = LLM.bind()
